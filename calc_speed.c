720beef0ae1f88be69cf6ffb4e186587ad338ced
Nikolay Esvyukov <evsukov@list.ru>
2020-11-10 13:01:34 +0300

    10/11/2020 13:01

--- calc_speed.c
+++ calc_speed.c
@@ -1,17 +1,58 @@
 #include "lem_in.h"
 
-static size_t	do_calc_speed(int ants_num,
-							size_t	*path_lens, size_t num_nodes)
+static void		case_minus()
+{
+
+}
+
+static size_t	do_calc_speed(int num, const size_t *arr, size_t len)
 {
 	size_t	result;
+	size_t	index;
+	size_t	area;
 
 	result = 0;
+	index = 0;
+	while (num > 0)
+	{
+		while (index < len - 1 && arr[index] == arr[index + 1])
+			++index;
+		area = (index + 1);
+		num -= area;
+	}
+	if (num < 0)
+		case_minus();
 	return (result);
 }
 
-static void		ft_sort(size_t *path_lens, size_t num_paths)
+static void		li_swap(size_t *a, size_t *b)
 {
+	size_t temp;
 
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+static void		li_sort(size_t *arr, size_t len)
+{
+	size_t	count;
+	size_t	index;
+
+	if (len <= 1)
+		return;
+	count = 0;
+	while (count < len)
+	{
+		index = 0;
+		while (index < len - 1)
+		{
+			if (arr[index] > arr[index + 1])
+				li_swap(&(arr[index]), &(arr[index + 1]));
+			++index;
+		}
+		++count;
+	}
 }
 
 size_t			calc_speed(t_graph *graph)
@@ -32,8 +73,7 @@ size_t			calc_speed(t_graph *graph)
 		paths_to_calc = paths_to_calc->next;
 		++index;
 	}
-	// !!! SORTING
-	ft_sort(path_lens, num_paths);
+	li_sort(path_lens, num_paths);
 	result = do_calc_speed(graph->ants_num, path_lens, num_paths);
 	FCNT((free(path_lens)));
     return (result);
