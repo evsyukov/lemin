a1edf9037673d8a84fee3f14fabecd3bfde2f26e
Sugar Manta <smanta@il-e5.msk.21-school.ru>
2020-11-20 21:34:15 +0300

    20/11/2020 21:33

--- print_result.c
+++ print_result.c
@@ -1,30 +1,5 @@
 #include "lem_in.h"
 
-void	print_ant_at_room(t_path *path)
-{
-	ft_putstr("L");
-	ft_putnbr(path->num_ant);
-	ft_putstr("-");
-	ft_putstr(path->node_name);
-}
-
-void 	print_one_from_path(t_paths *paths)
-{
-//	t_path	*begin_print;
-//	t_path	*next;
-//	t_path	*prev;
-//
-//	begin_print = paths->begin_print;
-//	if (begin_print->next != NULL)
-//	{
-//		print_ant_at_room(next);
-//	}
-//	else
-//	{
-//
-//	}
-}
-
 void	init_paths(t_paths *paths)
 {
 	while (paths != NULL)
@@ -35,7 +10,6 @@ void	init_paths(t_paths *paths)
 		paths = paths->next;
 	}
 }
-
 
 void	move_and_add_one(t_graph *graph, t_paths *paths,
 					size_t *count_ants, size_t *sum_ants)
@@ -49,21 +23,36 @@ void	move_and_add_one(t_graph *graph, t_paths *paths,
 	while (path != NULL)
 	{
 		path_next = path->next;
-		if (path->node == begin_path->node
-			&& *count_ants <= graph->ants_num)
+		// если текущая нода первая
+
+		if (path->node == begin_path->node)
 		{
-			path_next->num_ant = *count_ants;
-			*count_ants += 1;
-			*sum_ants += 1;
+			// и есть муравьи на старте
+			// то выделяем муравья и увеливаем счетчик номера муравья
+			if (*count_ants <= graph->ants_num)
+			{
+				path_next->num_ant = *count_ants;
+				*count_ants += 1;
+				*sum_ants += 1;
+			}
+			// иначе (если нет муравьев на старте)
+			// двигаем удказатель end_print вперед
+			else if (paths->end_print != NULL)
+				paths->end_print = paths->end_print->next;
 		}
+		// иначе, если текущая нода - последняя
+		// то уменьшаем сумму муравьев на всех путях
 		else if (path_next == NULL)
 			*sum_ants -= 1;
 		else
 			path_next->num_ant = path->num_ant;
 		path = path->prev;
 	}
+	// для перевода begin_print на новое место
+	// если не последний элемент, то двигаем вперед
 	if (paths->begin_print->next != NULL)
 		paths->begin_print = paths->begin_print->next;
+	// переназначение current на begin
 	paths->current_print = paths->begin_print;
 }
 
@@ -73,6 +62,7 @@ void	move_and_add_all(t_graph *graph, t_paths *paths,
 	t_paths	*paths_one;
 
 	paths_one = paths;
+	// перебираем все пути
 	while (paths_one != NULL)
 	{
 		move_and_add_one(graph, paths_one, count_ants, sum_ants);
@@ -80,12 +70,40 @@ void	move_and_add_all(t_graph *graph, t_paths *paths,
 	}
 }
 
-void	print_state(t_paths *paths)
+void	print_ant_at_room(t_path *path)
 {
-	while (paths != NULL)
+	ft_putstr("L");
+	ft_putnbr(path->num_ant);
+	ft_putstr("-");
+	ft_putstr(path->node_name);
+}
+
+void 	print_one_from_path(t_paths *paths)
+{
+	print_ant_at_room(paths->current_print);
+	paths->current_print = paths->current_print->prev;
+}
+
+void	print_state(t_paths *paths, size_t sum_ants)
+{
+	size_t	count_ants;
+	t_paths	*path;
+
+	count_ants = 0;
+	while (count_ants < sum_ants)
 	{
-		print_one_from_path(paths);
-		paths = paths->next;
+		path = paths;
+		while (path != NULL)
+		{
+			if (path->current_print != path->end_print)
+			{
+				// if (!count_ants)
+				print_one_from_path(path);
+				ft_putstr(" ");
+				++count_ants;
+			}
+			path = path->next;
+		}
 	}
 }
 
@@ -101,7 +119,11 @@ void	print_solution(t_graph *graph)
 	init_paths(paths);
 	while (count_ants <= graph->ants_num || sum_ants_at_paths > 0)
 	{
+		// двигаем вперед в каждом пути печатные указатели
+		// выделяем муравья, если они есть.
 		move_and_add_all(graph, paths, &count_ants, &sum_ants_at_paths);
-		print_state(paths);
+		// печатаем по одному муравью с каждого пути по элементно
+		print_state(paths, sum_ants_at_paths);
+		ft_putstr("\n");
 	}
 }
